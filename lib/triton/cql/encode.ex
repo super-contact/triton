0077fd2049d66f6f89d9b9f91b8a6d1a224c54a5
kernelgarden <kernelgarden@gmail.com>
2025-02-03 01:57:43 +0900

    feat(encode): blob 대응

--- lib/triton/cql/encode.ex
+++ lib/triton/cql/encode.ex
@@ -8,8 +8,17 @@ defmodule Triton.CQL.Encode do
 
   defp binary_value(v) do
     cond do
+      is_blob?(v) -> "0x#{Base.encode16(v)}"
       String.valid?(v) && String.contains?(v, ["'"]) -> "'#{String.replace(v, "'", "''")}'"
       true -> "'#{v}'"
+    end
+  end
+
+  defp is_blob?(binary) do
+    try do
+      !String.valid?(binary)
+    rescue
+      _ -> false
     end
   end
 end
